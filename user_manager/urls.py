098ac7acdaf1db93a9a8ce98c846c9302d4deb5e
Omikix <omikix@gmail.com>
2022-03-27 13:56:06 +0200

    Add registartion view

--- user_manager/urls.py
+++ user_manager/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from . import views
 
+urlpatterns = [
+    path('register/', views.register_view, name='register')
 ]
--- user_manager/views.py
+++ user_manager/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import NewUserForm
+
+
+def register_view(request):
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect("main:homepage")
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+    form = NewUserForm()
+    return render(request=request, template_name="user_manager/register.html", context={"register_form": form})
