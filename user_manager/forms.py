4804279c421fa27e15af580a0868c44e7bdb89ae
Omikix <omikix@gmail.com>
2022-04-09 15:45:07 +0200

    Add base logout template

--- user_manager/forms.py
+++ user_manager/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import password_validation
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django import forms
 
 User = get_user_model()
