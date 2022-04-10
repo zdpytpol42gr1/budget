fd319e6bb8ade219d54749dd951d8381373ce1ec
Jakub Wiejak <wiejoit@gmail.com>
2022-04-10 12:56:22 +0200

    home

--- project_budget/urls.py
+++ project_budget/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from user_manager.views import BaseView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", BaseView.as_view(), name='base'),
     path("user/", include("user_manager.urls")),
     path("balance/", include("balance.urls")),
 ]
